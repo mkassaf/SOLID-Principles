67984345a57bb339076c53bdb09d8ab9ccd2b9dd
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-10-13 19:36:08 +0300

    ✅ Keyboard.java

--- Dependency Inversion Principle/src/edu/najah/eng/solid/di/assignment/devices/Keyboard.java
+++ Dependency Inversion Principle/src/edu/najah/eng/solid/di/assignment/devices/Keyboard.java
@@ -1,4 +1,7 @@
 package edu.najah.eng.solid.di.assignment.devices;
+// The Dependency Inversion Principle (DIP) suggests that high-level modules should not depend on low-level modules; both should depend on
+//     abstractions. In your code, the WindowsMachine class depends directly on concrete implementations of Keyboard and Monitor, violating the DIP. 
+//     To resolve this, you can use dependency injection and introduce abstractions to decouple high-level modules from low-level modules.
 
 public class Keyboard {
     public void powerOn(){
@@ -8,4 +11,9 @@ public class Keyboard {
     public void powerOff(){
         System.out.println("Power off " + this.getClass().getName());
     }
+}
+// solution 
+public interface Keyboard {
+    void powerOn();
+    void powerOff();
 }
