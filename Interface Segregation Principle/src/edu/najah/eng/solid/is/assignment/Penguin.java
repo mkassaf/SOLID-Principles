a4b8965c47856a129752c8dc494c2f8bf865c993
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-10-13 19:24:46 +0300

    ✅ Penguin.java

--- Interface Segregation Principle/src/edu/najah/eng/solid/is/assignment/Penguin.java
+++ Interface Segregation Principle/src/edu/najah/eng/solid/is/assignment/Penguin.java
@@ -1,6 +1,6 @@
 package edu.najah.eng.solid.is.assignment;
 
-public class Penguin implements Bird {
+public class Penguin implements BirdCanswim,  BirdMolt  {
     String currentLocation;
     int numberOfFeathers;
 
@@ -11,11 +11,11 @@ public class Penguin implements Bird {
     public void molt() {
         this.numberOfFeathers -= 1;
     }
-
-    public void fly() {
-        System.err.println("Unsupported Operation Exception");
-        throw new UnsupportedOperationException();
-    }
+// since the Penguin cant fly we can delete and dont impements the fly() fun
+    // public void fly() {
+    //     System.err.println("Unsupported Operation Exception");
+    //     throw new UnsupportedOperationException();
+    // }
 
     public void swim() {
 
