a0558b5e5bf3ec51614634821d682e1455e46c4b
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-10-13 19:06:13 +0300

    Update LimitedCustomer.java

--- Liskov Substitution Principle/src/edu/najah/eng/solid/lsp/assignment/LimitedCustomer.java
+++ Liskov Substitution Principle/src/edu/najah/eng/solid/lsp/assignment/LimitedCustomer.java
@@ -1,5 +1,11 @@
 package edu.najah.eng.solid.lsp.assignment;
 
+
+// The Liskov Substitution Principle (LSP) is one of the SOLID principles, and it states that
+//     objects of a derived class should be able to replace objects of the base class without affecting the correctness of the program. 
+//     In your code, you have a base class Customer and a derived class LimitedCustomer. To make sure that you adhere to the Liskov Substitution Principle,
+//     you need to ensure that the derived class LimitedCustomer is a true subtype of the base class Customer.
+    
 public class LimitedCustomer extends Customer{
 
     private static final int MAX_ALLOWED_OFFER = 3;
