b639a30fb687d6c1214f31832aa84e0713cb5584
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-10-13 18:19:42 +0300

    Resolved  ✅  Invoice.java

--- Single Responsibility Principle/src/edu/najah/eng/solid/srp/assignment/Invoice.java
+++ Single Responsibility Principle/src/edu/najah/eng/solid/srp/assignment/Invoice.java
@@ -38,3 +38,41 @@ public class Invoice {
 
     }
 }
+// The methods in the Invoice class are tightly coupled and the add() method directly creates an instance of mailMessage .
+ public class Invoice {
+
+    private long amount;
+    private Date invoiceDate;
+
+    public Invoice(long amount, Date invoiceDate) {
+        this.amount = amount;
+        this.invoiceDate = invoiceDate;
+    }
+
+    public void add()  {
+        // connect to database
+        // execute insert command to add new record in database
+        //close the database connection
+        // Once Invoice has been added , send mail
+        MailMessage mailMessage = new MailMessage("MailAddressFrom", "MailAddressTo", "MailSubject", "MailBody");
+        sendEmails emailSender = new sendEmails();
+        sendEmails.sendEmail(mailMessage);
+    }
+    public void delete()  {
+        // connect to database
+        // execute delete command to delete the invoice from the database
+        //close the database connection
+    }
+ }
+
+// put sendEmail() method in separate class since its not related to Invoice class
+public class sendEmails {
+    public void sendEmail(MailMessage mailMessage)  {
+        if (mailMessage.isValidMessage()){
+            // Code for getting Email setting and send invoice mail
+        } else {
+            //show error message for the user
+        }
+
+    }
+
