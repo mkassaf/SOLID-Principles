917c1faf7c0de02d4f9b7f4b560773094cf74901
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-10-13 18:42:24 +0300

    Resolved ✅ MailMessage.java

--- Single Responsibility Principle/src/edu/najah/eng/solid/srp/assignment/MailMessage.java
+++ Single Responsibility Principle/src/edu/najah/eng/solid/srp/assignment/MailMessage.java
@@ -44,3 +44,46 @@ public class MailMessage {
         return true;
     }
 }
+// the constructor and isValidMessage() both validate th inputs and we have Complex Constructor so we can do the following 
+
+
+public MailMessage(String mailAddressFrom, String mailAddressTo, String mailSubject, String mailBody) {
+    this.mailAddressFrom=mailAddressFrom ;
+    this.mailAddressTo=mailAddressTo;
+    this.mailSubject=mailSubject;
+    this.mailBody=mailBody;
+}
+
+ public boolean isValidMessage() {
+        return isValidEmailAddress(mailAddressFrom) && isValidEmailAddress(mailAddressTo) && isValidSubject(mailSubject) && isValidBody(mailBody);
+    }
+
+ public boolean isValidMailAddressxFrom(String mailAddressFrom){
+        if(mailAddressFrom == null || mailAddressFrom.isEmpty() || !mailAddressFrom.contains("@") || !mailAddressFrom.contains(".")){
+            return false;
+            else{
+            return true
+                }
+        }
+     public boolean IsValidmailAddressTo(String mailAddressTo){
+ if(mailAddressTo == null ||  mailAddressTo.isEmpty() ||  !mailAddressTo.contains("@") ||  !mailAddressTo.contains(".")){
+            return false;
+      else{
+            return true
+                }
+        }
+
+     public boolean isValidmailSubject(String mailSubject){
+ if(mailSubject == null ||  mailSubject.isEmpty() ||  mailSubject.length() > 30){
+            return false;
+      else{
+            return true
+                }
+        }
+         public boolean isValidmailBody(String mailBody){
+               if(mailBody == null || mailBody.isEmpty() ){
+            return false;
+                    else{
+            return true
+                }
+        }
